075fed5382e59cb392b4ef94ddd078d9c39d8c6d
Andrew Zheng <andzheng@gmail.com>
2022-02-18 14:19:14 -0500

    Added Investments dataclass, database, & DAO

---
+++ app/src/main/java/com/example/wutmygainz/database/InvestmentsDatabase.kt
@@ -0,0 +1,32 @@
+package com.example.wutmygainz.database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [Investments::class], version = 1, exportSchema = false)
+abstract class InvestmentsDatabase: RoomDatabase() {
+    abstract val investmentDatabaseDao: InvestmentsDatabaseDAO
+
+    companion object {
+
+        private var INSTANCE: InvestmentsDatabase? = null
+
+        fun getInstance(context: Context): InvestmentsDatabase {
+            synchronized(this) {
+                var instance = INSTANCE
+                if (instance == null) {
+                    instance = Room.databaseBuilder(
+                        context.applicationContext,
+                        InvestmentsDatabase::class.java,
+                        "investment_database")
+                        .fallbackToDestructiveMigration()
+                        .build()
+                    INSTANCE = instance
+                }
+                return instance
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/wutmygainz/database/InvestmentsDatabaseDAO.kt
@@ -0,0 +1,21 @@
+package com.example.wutmygainz.database
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+
+@Dao
+interface InvestmentsDatabaseDAO {
+    @Insert
+    fun insert(investments: Investments)
+    @Update
+    fun update(investments: Investments)
+    @Query("SELECT * from cryptocurrency_investment_table WHERE investmentId = :key")
+    fun get(key: Long): Investments?
+    @Query("DELETE FROM cryptocurrency_investment_table")
+    fun deleteAll()
+    @Query("SELECT * FROM cryptocurrency_investment_table ORDER BY purchase_date DESC")
+    fun getAllInvestments(): LiveData<List<Investments>>
+}
